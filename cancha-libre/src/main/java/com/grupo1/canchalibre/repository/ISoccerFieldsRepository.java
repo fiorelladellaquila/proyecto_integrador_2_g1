c03f6537a5bca8b93e008408d2df94d0abe37919
integraceferina <yesicamelina882@gmail.com>
2023-11-09 17:26:23 -0300

    Soccer Fields method

---
+++ cancha-libre/src/main/java/com/grupo1/canchalibre/repository/ISoccerFieldsRepository.java
@@ -0,0 +1,8 @@
+package com.grupo1.canchalibre.repository;
+
+import com.grupo1.canchalibre.entity.SoccerFields;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ISoccerFieldsRepository extends JpaRepository<SoccerFields, Long> {
+
+}

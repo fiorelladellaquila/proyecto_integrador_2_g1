11da0aa329b79ff6f7a70dcf51d75c6f6f9127fa
Erika Sandoval <erikasandoval-09@hotmail.com>
2023-11-16 21:06:24 -0300

    Fix estilos login

--- cancha_libre/components/login/template/LoginComponent.tsx
+++ cancha_libre/components/login/template/LoginComponent.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import LayoutGeneral from '../../layouts/layout-general';
-import LoginFormContainer from '../LoginFormContainer';
-import ImageContainer from '../ImageContainer';
-import {
-  Container,
-  ContainerImage,
-  ContainerLoginForm,
-} from './loginComponent.style';
-import { Provider } from 'react-redux';
-import store from '../../../redux/store';
+import React from "react";
+import LayoutGeneral from "../../layouts/layout-general";
+import LoginFormContainer from "../LoginFormContainer";
+import { Container } from "./loginComponent.style";
+import { Provider } from "react-redux";
+import store from "../../../redux/store";
+import { ThemeProvider } from "@mui/system";
+import { theme } from "@/styles/material-theme";
 
 const Login = () => {
   return (
-    <Provider store={store}>
-        <LayoutGeneral title={'Login'}>
-      <Container>
-          <LoginFormContainer />
-      </Container>
-      </LayoutGeneral>
-    </Provider>
-    
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <LayoutGeneral title={"Login"}>
+          <Container>
+            <LoginFormContainer />
+          </Container>
+        </LayoutGeneral>
+      </Provider>
+    </ThemeProvider>
   );
 };
 

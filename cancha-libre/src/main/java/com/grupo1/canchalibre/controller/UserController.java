a82526c842f2ad5f2c6a2e6fbbb0dae929c04875
PatoAlloco <patoalloco13@hotmail.com>
2023-11-20 18:56:18 -0300

    gestion de usuarios completa mas cors

---
+++ cancha-libre/src/main/java/com/grupo1/canchalibre/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.grupo1.canchalibre.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    @PostMapping(value = "/login")
+    public String login(){
+        return "rgrvsv";
+    }
+}
---
+++ cancha-libre/src/main/java/com/grupo1/canchalibre/security/AuthenticationService.java
@@ -0,0 +1,51 @@
+package com.grupo1.canchalibre.security;
+
+import com.grupo1.canchalibre.dto.AuthResponseDTO;
+import com.grupo1.canchalibre.dto.UserLoginDTO;
+import com.grupo1.canchalibre.dto.UserRegisterDTO;
+import com.grupo1.canchalibre.entity.Rol;
+import com.grupo1.canchalibre.entity.User;
+import com.grupo1.canchalibre.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final IUserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponseDTO login(UserLoginDTO request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user=userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token=jwtService.getToken(user);
+        return AuthResponseDTO.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthResponseDTO register(UserRegisterDTO request) {
+        User user = User.builder()
+                .username(request.getUsername())
+                .name(request.getName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .phone(request.getPhone())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .rol(Rol.ROLE_USER)
+                .build();
+
+        userRepository.save(user);
+
+        return AuthResponseDTO.builder()
+                .token(jwtService.getToken(user))
+                .build();
+
+    }
+}

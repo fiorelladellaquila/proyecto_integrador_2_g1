427e79ef0b5813e73e5a38c20141ed8c8f198138
PatoAlloco <patoalloco13@hotmail.com>
2023-11-23 01:13:28 -0300

    gestion usuarios y renovar password

--- cancha-libre/src/main/java/com/grupo1/canchalibre/repository/IUserRepository.java
+++ cancha-libre/src/main/java/com/grupo1/canchalibre/repository/IUserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }

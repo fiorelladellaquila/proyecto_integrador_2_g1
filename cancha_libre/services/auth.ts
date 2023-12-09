29bfee44c56dd4e3aaad8e9909fc182bdca1937c
Nicolas Montero Sabelli <nico.monterosabelli@gmail.com>
2023-12-09 20:28:09 -0300

    CORS globales en MS resuelto, falta cors por gateway

--- cancha_libre/services/auth.ts
+++ cancha_libre/services/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://run.mocky.io/v3/';
-// const BASE_URL = 'http://localhost:8080/';
+//const BASE_URL = 'https://run.mocky.io/v3/';
+const BASE_URL = 'http://localhost:8080/';
 
 const apiService = axios.create({
   baseURL: BASE_URL,
@@ -13,10 +13,10 @@ interface LoginResponse {
     username: string
   }
 
-export const login = async (email: string, password: string):  Promise<LoginResponse> => {
+export const login = async (username: string, password: string):  Promise<LoginResponse> => {
   try {
-    const response = await apiService.post('3a273d40-8aa9-40dc-b69b-73c857c22298', {  //  200 => '3a273d40-8aa9-40dc-b69b-73c857c22298', 403 => '79eed8d6-9adb-42cc-83f8-00f6309bc57e',  endpoinyt => 'canchas/auth/login' 
-      email,
+    const response = await apiService.post('canchas/auth/login', {  //  200 => '3a273d40-8aa9-40dc-b69b-73c857c22298', 403 => '79eed8d6-9adb-42cc-83f8-00f6309bc57e',  endpoinyt => 'canchas/auth/login' 
+      username,
       password,
     });
     console.log('response.data',)
